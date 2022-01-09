8c145c88653e0800ffa1fc5c5f8c50ae919d90d8
Andreas Rozek <A.Rozek@gmx.de>
2022-01-09 11:32:28 +0100

    Create ClockBackground.js

---
+++ ClockBackground.js
@@ -0,0 +1,19 @@
+(function () {
+  let currentBitmap, currentImage;
+
+  exports.draw = function draw (Settings) {
+    let Bitmap = Settings.Bitmap;
+    if (Bitmap != currentBitmap) {
+      currentBitmap = Bitmap;
+      if (currentBitmap == null) {
+        currentImage = undefined;
+      } else {
+        currentImage = require('Storage').read(currentBitmap);
+      }
+
+      g.clear();
+      if (currentImage != null) { g.drawImage(currentImage, 0,0); }
+    }
+  };
+})();
+
